e472490518ae80d6520cb157f41f2f5e44e67780
Ashuv786 <ashu.verma93@outlook.com>
2022-03-11 17:36:02 +0530

    first commit

---
+++ cloudwatch.tf
@@ -0,0 +1,9 @@
+/* Cloudwatch Log-group Definition For Storing The ECS Logs */
+resource "aws_cloudwatch_log_group" "logs" {
+  name              = "/fargate/service/app-${terraform.workspace}"
+  retention_in_days = "90"
+  tags = merge(
+    {
+      "Name" : "app-ecs-loggroup-${terraform.workspace}"
+  }, var.default_tags)
+}
---
+++ internetgateway.tf
@@ -0,0 +1,10 @@
+/*
+  Internet-gateway Definition
+*/
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = merge({
+    Name = "ig-lamp-${terraform.workspace}"
+  }, var.default_tags)
+}
